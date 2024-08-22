b895c45516eb391589c92f98d911af5816ad140e
Jebb Burditt <jebb.burditt@gmail.com>
2024-08-21 21:39:41 -0700

    fix program and contract id query parameters

--- Resources/PaymentRepository.cs
+++ Resources/PaymentRepository.cs
@@ -8,28 +8,36 @@ public class PaymentRepository(DatabaseContext databaseContext, IMapper mapper) 
 {
     public PaymentResult Query(PaymentQuery paymentQuery)
     {
-        var query = 
-            from payment in databaseContext.Vsd_PaymentSet
-            join program in databaseContext.Vsd_ProgramSet on payment.Vsd_ProgramId.Id equals program.Vsd_ProgramId
-            join contract in databaseContext.Vsd_ContractSet on payment.Vsd_ContractId.Id equals contract.Vsd_ContractId
-            select new { Payment = payment, Program = program, Contract = contract };
+        //var query = 
+        //    from payment in databaseContext.Vsd_PaymentSet
+        //    join program in databaseContext.Vsd_ProgramSet on payment.Vsd_ProgramId.Id equals program.Vsd_ProgramId
+        //    join contract in databaseContext.Vsd_ContractSet on payment.Vsd_ContractId.Id equals contract.Vsd_ContractId
+        //    //where paymentQuery.ProgramId == null || program.Id == paymentQuery.ProgramId
+        //    //where paymentQuery.ContractId == null || contract.Id == paymentQuery.ContractId
+        //    //where paymentQuery.ExcludeStatusCodes == null || (payment.StatusCode == null || !paymentQuery.ExcludeStatusCodes.Contains((PaymentStatusCode)payment.StatusCode))
+        //    select new { Payment = payment, Program = program, Contract = contract };
+        var query = databaseContext.Vsd_PaymentSet;
 
-        //if (paymentQuery.ProgramId != null)
-        //{
+        if (paymentQuery.ProgramId != null)
+        {
+        //    query = from payment in query.
         //    query = query.Where(c => c.Program.Id == paymentQuery.ProgramId);
-        //}
+            query = query.Where(p => p.Vsd_ProgramId.Id == paymentQuery.ProgramId);
+        }
 
-        //if (paymentQuery.ContractId != null)
-        //{
-        //    query = query.Where(c => c.Contract.Id == paymentQuery.ContractId);
-        //}
+        if (paymentQuery.ContractId != null)
+        {
+            //    query = query.Where(c => c.Contract.Id == paymentQuery.ContractId);
+            query = query.Where(p => p.Vsd_ContractId.Id == paymentQuery.ContractId);
+        }
 
-        //if (paymentQuery.ExcludeStatusCodes != null)
-        //{
-        //    query = query.Where(x => x.Payment.StatusCode == null || paymentQuery.ExcludeStatusCodes.Contains((PaymentStatusCode)x.Payment.StatusCode));
-        //}
+        if (paymentQuery.ExcludeStatusCodes != null)
+        {
+            //    query = query.Where(x => x.Payment.StatusCode == null || paymentQuery.ExcludeStatusCodes.Contains((PaymentStatusCode)x.Payment.StatusCode));
+            //query = query.Where(p => p.StatusCode == null || !paymentQuery.ExcludeStatusCodes.Contains((PaymentStatusCode)p.StatusCode));
+        }
 
-        var queryResults = query.ToList().Select(x => x.Payment);
+        var queryResults = query.ToList();//.Select(x => x.Payment);
         var payments = mapper.Map<IEnumerable<Payment>>(queryResults);
         return new PaymentResult(payments);
     }
